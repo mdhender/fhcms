12fd4a06826e925e6335921a353a9b5bbc5b01b2
Michael Henderson <mdhender@mdhender.com>
2021-07-16 09:21:22 -0600

    cleaned up header

---
+++ fh.go
@@ -0,0 +1,251 @@
+/*
+ * Far Horizons Engine
+ * Copyright (C) 2021  Michael D Henderson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package main
+
+const STANDARD_NUMBER_OF_SPECIES = 15      /* A standard game has 15 species. */
+const STANDARD_NUMBER_OF_STAR_SYSTEMS = 90 /* A standard game has 90 star systems. */
+const STANDARD_GALACTIC_RADIUS = 20        /* A standard game has a galaxy with a radius of 20 parsecs. */
+
+/* Minimum and maximum values for a galaxy. */
+const MIN_SPECIES = 1
+const MAX_SPECIES = 100
+const MIN_STARS = 12
+const MAX_STARS = 1000
+const MIN_RADIUS = 6
+const MAX_RADIUS = 50
+const MAX_DIAMETER = 2 * MAX_RADIUS
+const MAX_PLANETS = 9 * MAX_STARS
+
+const HP_AVAILABLE_POP = 1500
+
+const NUM_EXTRA_NAMPLAS = 50
+const NUM_EXTRA_SHIPS = 100
+
+const MAX_LOCATIONS = 10000
+
+/* Assume at least 32 bits per long word. */
+const NUM_CONTACT_WORDS = ((MAX_SPECIES - 1) / 32) + 1
+
+/* Star ttypes. */
+const DWARF = 1
+const DEGENERATE = 2
+const MAIN_SEQUENCE = 3
+const GIANT = 4
+
+/* Star Colors. */
+const BLUE = 1
+const BLUE_WHITE = 2
+const WHITE = 3
+const YELLOW_WHITE = 4
+const YELLOW = 5
+const ORANGE = 6
+const RED = 7
+
+/* Gases in planetary atmospheres. */
+const H2 = 1   /* Hydrogen */
+const CH4 = 2  /* Methane */
+const HE = 3   /* Helium */
+const NH3 = 4  /* Ammonia */
+const N2 = 5   /* Nitrogen */
+const CO2 = 6  /* Carbon Dioxide */
+const O2 = 7   /* Oxygen */
+const HCL = 8  /* Hydrogen Chloride */
+const CL2 = 9  /* Chlorine */
+const F2 = 10  /* Fluorine */
+const H2O = 11 /* Steam */
+const SO2 = 12 /* Sulfur Dioxide */
+const H2S = 13 /* Hydrogen Sulfide */
+
+/* Tech level ids. */
+const MI = 0 /* Mining tech level. */
+const MA = 1 /* Manufacturing tech level. */
+const ML = 2 /* Military tech level. */
+const GV = 3 /* Gravitics tech level. */
+const LS = 4 /* Life Support tech level. */
+const BI = 5 /* Biology tech level. */
+
+/* Item IDs. */
+const RM = 0   /* Raw Material Units. */
+const PD = 1   /* Planetary Defense Units. */
+const SU = 2   /* Starbase Units. */
+const DR = 3   /* Damage Repair Units. */
+const CU = 4   /* Colonist Units. */
+const IU = 5   /* Colonial Mining Units. */
+const AU = 6   /* Colonial Manufacturing Units. */
+const FS = 7   /* Fail-Safe Jump Units. */
+const JP = 8   /* Jump Portal Units. */
+const FM = 9   /* Forced Misjump Units. */
+const FJ = 10  /* Forced Jump Units. */
+const GT = 11  /* Gravitic Telescope Units. */
+const FD = 12  /* Field Distortion Units. */
+const TP = 13  /* Terraforming Plants. */
+const GW = 14  /* Germ Warfare Bombs. */
+const SG1 = 15 /* Mark-1 Auxiliary Shield Generators. */
+const SG2 = 16 /* Mark-2. */
+const SG3 = 17 /* Mark-3. */
+const SG4 = 18 /* Mark-4. */
+const SG5 = 19 /* Mark-5. */
+const SG6 = 20 /* Mark-6. */
+const SG7 = 21 /* Mark-7. */
+const SG8 = 22 /* Mark-8. */
+const SG9 = 23 /* Mark-9. */
+const GU1 = 24 /* Mark-1 Auxiliary Gun Units. */
+const GU2 = 25 /* Mark-2. */
+const GU3 = 26 /* Mark-3. */
+const GU4 = 27 /* Mark-4. */
+const GU5 = 28 /* Mark-5. */
+const GU6 = 29 /* Mark-6. */
+const GU7 = 30 /* Mark-7. */
+const GU8 = 31 /* Mark-8. */
+const GU9 = 32 /* Mark-9. */
+const X1 = 33  /* Unassigned. */
+const X2 = 34  /* Unassigned. */
+const X3 = 35  /* Unassigned. */
+const X4 = 36  /* Unassigned. */
+const X5 = 37  /* Unassigned. */
+
+const MAX_ITEMS = 38 /* Always bump this up to a multiple of two. Don't forget to make room for zeroth element! */
+
+/* Status codes for named planets. These are logically ORed together. */
+const HOME_PLANET = 1
+const COLONY = 2
+const POPULATED = 8
+const MINING_COLONY = 16
+const RESORT_COLONY = 32
+const DISBANDED_COLONY = 64
+
+/* Ship classes. */
+const PB = 0  /* Picketboat. */
+const CT = 1  /* Corvette. */
+const ES = 2  /* Escort. */
+const DD = 3  /* Destroyer. */
+const FG = 4  /* Frigate. */
+const CL = 5  /* Light Cruiser. */
+const CS = 6  /* Strike Cruiser. */
+const CA = 7  /* Heavy Cruiser. */
+const CC = 8  /* Command Cruiser. */
+const BC = 9  /* Battlecruiser. */
+const BS = 10 /* Battleship. */
+const DN = 11 /* Dreadnought. */
+const SD = 12 /* Super Dreadnought. */
+const BM = 13 /* Battlemoon. */
+const BW = 14 /* Battleworld. */
+const BR = 15 /* Battlestar. */
+const BA = 16 /* Starbase. */
+const TR = 17 /* Transport. */
+
+const NUM_SHIP_CLASSES = 18
+
+/* Ship ttypes. */
+const FTL = 0
+const SUB_LIGHT = 1
+const STARBASE = 2
+
+/* Ship status codes. */
+const UNDER_CONSTRUCTION = 0
+const ON_SURFACE = 1
+const IN_ORBIT = 2
+const IN_DEEP_SPACE = 3
+const JUMPED_IN_COMBAT = 4
+const FORCED_JUMP = 5
+
+/* Interspecies transactions. */
+
+const MAX_TRANSACTIONS = 1000
+
+const EU_TRANSFER = 1
+const MESSAGE_TO_SPECIES = 2
+const BESIEGE_PLANET = 3
+const SIEGE_EU_TRANSFER = 4
+const TECH_TRANSFER = 5
+const DETECTION_DURING_SIEGE = 6
+const SHIP_MISHAP = 7
+const ASSIMILATION = 8
+const INTERSPECIES_CONSTRUCTION = 9
+const TELESCOPE_DETECTION = 10
+const ALIEN_JUMP_PORTAL_USAGE = 11
+const KNOWLEDGE_TRANSFER = 12
+const LANDING_REQUEST = 13
+const LOOTING_EU_TRANSFER = 14
+const ALLIES_ORDER = 15
+
+/* Command codes. */
+const UNDEFINED = 0
+const ALLY = 1
+const AMBUSH = 2
+const ATTACK = 3
+const AUTO = 4
+const BASE = 5
+const BATTLE = 6
+const BUILD = 7
+const CONTINUE = 8
+const DEEP = 9
+const DESTROY = 10
+const DEVELOP = 11
+const DISBAND = 12
+const END = 13
+const ENEMY = 14
+const ENGAGE = 15
+const ESTIMATE = 16
+const HAVEN = 17
+const HIDE = 18
+const HIJACK = 19
+const IBUILD = 20
+const ICONTINUE = 21
+const INSTALL = 22
+const INTERCEPT = 23
+const JUMP = 24
+const LAND = 25
+const MESSAGE = 26
+const MOVE = 27
+const NAME = 28
+const NEUTRAL = 29
+const ORBIT = 30
+const PJUMP = 31
+const PRODUCTION = 32
+const RECYCLE = 33
+const REPAIR = 34
+const RESEARCH = 35
+const SCAN = 36
+const SEND = 37
+const SHIPYARD = 38
+const START = 39
+const SUMMARY = 40
+const SURRENDER = 41
+const TARGET = 42
+const TEACH = 43
+const TECH = 44
+const TELESCOPE = 45
+const TERRAFORM = 46
+const TRANSFER = 47
+const UNLOAD = 48
+const UPGRADE = 49
+const VISITED = 50
+const WITHDRAW = 51
+const WORMHOLE = 52
+const ZZZ = 53
+const NUM_COMMANDS = ZZZ + 1
+
+/* Constants needed for parsing. */
+const UNKNOWN = 0
+const TECH_ID = 1
+const ITEM_CLASS = 2
+const SHIP_CLASS = 3
+const PLANET_ID = 4
+const SPECIES_ID = 5
--- globals.go
+++ globals.go
@@ -20,73 +20,206 @@ package main
 
 import "io"
 
+/* Global data used in most or all programs. */
+
 var (
-	append_log              [MAX_SPECIES]byte
-	battle_base             *battle_data
-	c_nampla                [MAX_SPECIES]*nampla_data
-	c_ship                  [MAX_SPECIES]*ship_data
-	c_species               [MAX_SPECIES]*species_data
-	doing_production        int
-	end_of_file             int
-	first_pass              int
-	fleet_percent_cost      int
-	galaxy                  galaxy_data
+	append_log   [MAX_SPECIES]byte
+	battle_base  *battle_data
+	c_nampla     [MAX_SPECIES]*nampla_data
+	color_char   = []byte(" OBAFGKM")
+	command_abbr = []string{ // warning: code assumes [NUM_COMMANDS][4]byte
+		"   ", "ALL", "AMB", "ATT", "AUT", "BAS", "BAT", "BUI", "CON",
+		"DEE", "DES", "DEV", "DIS", "END", "ENE", "ENG", "EST", "HAV",
+		"HID", "HIJ", "IBU", "ICO", "INS", "INT", "JUM", "LAN", "MES",
+		"MOV", "NAM", "NEU", "ORB", "PJU", "PRO", "REC", "REP", "RES",
+		"SCA", "SEN", "SHI", "STA", "SUM", "SUR", "TAR", "TEA", "TEC",
+		"TEL", "TER", "TRA", "UNL", "UPG", "VIS", "WIT", "WOR", "ZZZ",
+	}
+	command_name = []string{ // warning: code assumes [NUM_COMMANDS][16]byte
+		"Undefined", "Ally", "Ambush", "Attack", "Auto", "Base",
+		"Battle", "Build", "Continue", "Deep", "Destroy", "Develop",
+		"Disband", "End", "Enemy", "Engage", "Estimate", "Haven",
+		"Hide", "Hijack", "Ibuild", "Icontinue", "Install", "Intercept",
+		"Jump", "Land", "Message", "Move", "Name", "Neutral", "Orbit",
+		"Pjump", "Production", "Recycle", "Repair", "Research", "Scan",
+		"Send", "Shipyard", "Start", "Summary", "Surrender", "Target",
+		"Teach", "Tech", "Telescope", "Terraform", "Transfer", "Unload",
+		"Upgrade", "Visited", "Withdraw", "Wormhole", "ZZZ",
+	}
+	c_ship             [MAX_SPECIES]*ship_data_
+	c_species          [MAX_SPECIES]*species_data
+	data_in_memory     [MAX_SPECIES]int
+	data_modified      [MAX_SPECIES]int
+	doing_production   int
+	end_of_file        int
+	first_pass         int
+	fleet_percent_cost int
+	galaxy             galaxy_data
+	gas_string         = []string{ // warning: code assumes [14][4]byte
+		"   ", "H2", "CH4", "He", "NH3", "N2", "CO2", "O2", "HCl", "Cl2", "F2", "H2O", "SO2", "H2S",
+	}
 	header_printed          int
 	home_planet             *planet_data
 	ignore_field_distorters int
 	input_file              io.Reader
 	input_line              [256]byte
 	input_line_pointer      *[]byte
-	just_opened_file        int
-	last_random             int
-	loc                     [MAX_LOCATIONS]sp_loc_data
-	log_file                io.Writer
-	log_stdout              int
-	make_enemy              [MAX_SPECIES][MAX_SPECIES]byte
-	nampla1_base            *nampla_data
-	nampla2_base            *nampla_data
-	nampla_base             *nampla_data
-	nampla_index            int
-	nampla                  *nampla_data
-	next_nampla_index       int
-	next_nampla             *nampla_data
-	num_intercepts          int
-	num_locs                int
-	num_planets             int
-	num_stars               int
-	num_transactions        int
-	orders_file             io.Writer
-	original_line           [256]byte
-	planet_base             *planet_data
-	planet_data_modified    int
-	planet                  *planet_data
-	pn                      int
-	post_arrival_phase      bool // warning: must be initialized to true for PostArrivalMain
-	printing_alien          int
-	production_done         [1000]byte
-	prompt_gm               int
-	report_file             io.Writer
-	ship1_base              *ship_data
-	ship2_base              *ship_data
-	ship_already_listed     [5000]byte
-	ship_base               *ship_data
-	ship_index              int
-	ship                    *ship_data
-	species_index           int
-	species_number          int
-	species                 *species_data
-	sp_tech_level           [6]int
-	star_base               *star_data
-	star_data_modified      int
-	star                    *star_data
-	strike_phase            bool
-	test_mode               bool
-	transaction             [MAX_TRANSACTIONS]trans_data
-	truncate_name           bool
-	upper_name              []byte
-	value                   int
-	verbose_mode            bool
-	x                       int
-	y                       int
-	z                       int
+	item_abbr               = []string{ // warning: code assumes [MAX_ITEMS][4]byte
+		"RM", "PD", "SU", "DR", "CU", "IU", "AU", "FS",
+		"JP", "FM", "FJ", "GT", "FD", "TP", "GW", "SG1",
+		"SG2", "SG3", "SG4", "SG5", "SG6", "SG7", "SG8", "SG9",
+		"GU1", "GU2", "GU3", "GU4", "GU5", "GU6", "GU7", "GU8",
+		"GU9", "X1", "X2", "X3", "X4", "X5",
+	}
+	item_carry_capacity = []int{ // warning: code assumes [MAX_ITEMS]int
+		1, 3, 20, 1, 1, 1, 1, 1,
+		10, 5, 5, 20, 1, 100, 100, 5,
+		10, 15, 20, 25, 30, 35, 40, 45,
+		5, 10, 15, 20, 25, 30, 35, 40,
+		45, 9999, 9999, 9999, 9999, 9999,
+	}
+	item_cost = []int{ // warning: code assumes [MAX_ITEMS]int
+		1, 1, 110, 50, 1, 1, 1, 25,
+		100, 100, 125, 500, 50, 50000, 1000, 250,
+		500, 750, 1000, 1250, 1500, 1750, 2000, 2250,
+		250, 500, 750, 1000, 1250, 1500, 1750, 2000,
+		2250, 9999, 9999, 9999, 9999, 9999,
+	}
+	item_critical_tech = []int{ // warning: code assumes [MAX_ITEMS]int
+		MI, ML, MA, MA, LS, MI, MA, GV,
+		GV, GV, GV, GV, LS, BI, BI, LS,
+		LS, LS, LS, LS, LS, LS, LS, LS,
+		ML, ML, ML, ML, ML, ML, ML, ML,
+		ML, 99, 99, 99, 99, 99,
+	}
+	item_name = []string{ // warning: code assumes [MAX_ITEMS][32]byte
+		"Raw Material Unit",
+		"Planetary Defense Unit",
+		"Starbase Unit",
+		"Damage Repair Unit",
+		"Colonist Unit",
+		"Colonial Mining Unit",
+		"Colonial Manufacturing Unit",
+		"Fail-Safe Jump Unit",
+		"Jump Portal Unit",
+		"Forced Misjump Unit",
+		"Forced Jump Unit",
+		"Gravitic Telescope Unit",
+		"Field Distortion Unit",
+		"Terraforming Plant",
+		"Germ Warfare Bomb",
+		"Mark-1 Shield Generator",
+		"Mark-2 Shield Generator",
+		"Mark-3 Shield Generator",
+		"Mark-4 Shield Generator",
+		"Mark-5 Shield Generator",
+		"Mark-6 Shield Generator",
+		"Mark-7 Shield Generator",
+		"Mark-8 Shield Generator",
+		"Mark-9 Shield Generator",
+		"Mark-1 Gun Unit",
+		"Mark-2 Gun Unit",
+		"Mark-3 Gun Unit",
+		"Mark-4 Gun Unit",
+		"Mark-5 Gun Unit",
+		"Mark-6 Gun Unit",
+		"Mark-7 Gun Unit",
+		"Mark-8 Gun Unit",
+		"Mark-9 Gun Unit",
+		"X1 Unit",
+		"X2 Unit",
+		"X3 Unit",
+		"X4 Unit",
+		"X5 Unit",
+	}
+	item_tech_requirment = []int{ // warning [MAX_ITEMS]int
+		1, 1, 20, 30, 1, 1, 1, 20,
+		25, 30, 40, 50, 20, 40, 50, 10,
+		20, 30, 40, 50, 60, 70, 80, 90,
+		10, 20, 30, 40, 50, 60, 70, 80,
+		90, 999, 999, 999, 999, 999,
+	}
+	just_opened_file     int
+	last_random          int
+	loc                  [MAX_LOCATIONS]sp_loc_data
+	log_file             io.Writer
+	log_stdout           int
+	make_enemy           [MAX_SPECIES][MAX_SPECIES]byte
+	namp_data            [MAX_SPECIES]*nampla_data
+	nampla1_base         *nampla_data
+	nampla2_base         *nampla_data
+	nampla_base          *nampla_data
+	nampla_index         int
+	nampla               *nampla_data
+	next_nampla_index    int
+	next_nampla          *nampla_data
+	num_intercepts       int
+	num_locs             int
+	num_new_namplas      [MAX_SPECIES]int
+	num_new_ships        [MAX_SPECIES]int
+	num_planets          int
+	num_stars            int
+	num_transactions     int
+	orders_file          io.Writer
+	original_line        [256]byte
+	planet_base          *planet_data
+	planet_data_modified int
+	planet               *planet_data
+	pn                   int
+	post_arrival_phase   bool // warning: must be initialized to true for PostArrivalMain
+	printing_alien       int
+	production_done      [1000]byte
+	prompt_gm            int
+	report_file          io.Writer
+	ship1_base           *ship_data_
+	ship2_base           *ship_data_
+	ship_abbr            = []string{ // warning: code assumes [NUM_SHIP_CLASSES][4]byte
+		"PB", "CT", "ES", "FF", "DD", "CL", "CS",
+		"CA", "CC", "BC", "BS", "DN", "SD", "BM",
+		"BW", "BR", "BA", "TR",
+	}
+	ship_already_listed [5000]byte
+	ship_base           *ship_data_
+	ship_cost           = []int{ // warning: code assumes [NUM_SHIP_CLASSES]int
+		100, 200, 500, 1000, 1500, 2000, 2500,
+		3000, 3500, 4000, 4500, 5000, 5500, 6000,
+		6500, 7000, 100, 100,
+	}
+	ship_data    [MAX_SPECIES]*ship_data_
+	ship_index   int
+	ship         *ship_data_
+	ship_tonnage = []int{ // warning: code assumes [NUM_SHIP_CLASSES]int
+		1, 2, 5, 10, 15, 20, 25,
+		30, 35, 40, 45, 50, 55, 60,
+		65, 70, 1, 1,
+	}
+	ship_ttype = []string{ // warning: code assumes [3][2]byte
+		"", "S", "S",
+	}
+	size_char          []byte = []byte("0123456789")
+	spec_data          [MAX_SPECIES]*species_data
+	species_index      int
+	species_number     int
+	species            *species_data
+	sp_tech_level      [6]int
+	star_base          *star_data
+	star_data_modified int
+	star               *star_data
+	strike_phase       bool
+	tech_abbr          = []string{ // warning: code assumes [6][4]byte
+		"MI", "MA", "ML", "GV", "LS", "BI",
+	}
+	tech_name = []string{ // warning: code assumes [6][16]byte
+		"Mining", "Manufacturing", "Military", "Gravitics", "Life Support", "Biology",
+	}
+	test_mode     bool
+	transaction   [MAX_TRANSACTIONS]trans_data
+	truncate_name bool
+	ttype_char    []byte = []byte(" dD g")
+	upper_name    []byte
+	value         int
+	verbose_mode  bool
+	x             int
+	y             int
+	z             int
 )
