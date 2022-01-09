1f407eb067a875753924dc34536f109374169bce
Michael Henderson <mdhender@mdhender.com>
2022-01-09 09:33:19 -0700

    tried a render functions

--- internal/reactor/home.go
+++ internal/reactor/home.go
@@ -35,29 +35,32 @@ func (s *Server) homeGetIndex(sf SiteStore, templates string) http.HandlerFunc {
 		}
 		u := s.currentUser(r)
 		log.Printf("[reactor] homeGetIndex: u.id %d\n", u.Id)
-
-		t, err := template.ParseFiles(filepath.Join(templates, "site.layout.gohtml"), filepath.Join(templates, "fragments", "navbar.gohtml"), filepath.Join(templates, "fragments", "footer.gohtml"), filepath.Join(templates, "home.index.gohtml"))
-		if err != nil {
-			log.Printf("[reactor] homeGetIndex: %+v\n", err)
+		if b, err := renderHome(sf, u, templates); err != nil {
 			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-			return
+		} else {
+			w.Header().Set("Content-Type", "text/html; charset=utf-8")
+			_, _ = w.Write(b)
 		}
-
-		var payload struct {
-			Site    models.Site
-			Account models.Account
-		}
-		payload.Site, _ = sf.FetchSite()
-		payload.Account = u
-
-		b := &bytes.Buffer{}
-		if err = t.ExecuteTemplate(b, "layout", payload); err != nil {
-			log.Printf("[reactor] homeGetIndex: %+v\n", err)
-			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-			return
-		}
-
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		_, _ = w.Write(b.Bytes())
 	}
+}
+
+func renderHome(sf SiteStore, a models.Account, templates string) ([]byte, error) {
+	t, err := template.ParseFiles(filepath.Join(templates, "site.layout.gohtml"), filepath.Join(templates, "fragments", "navbar.gohtml"), filepath.Join(templates, "fragments", "footer.gohtml"), filepath.Join(templates, "home.index.gohtml"))
+	if err != nil {
+		log.Printf("[reactor] renderHome: %+v\n", err)
+		return nil, err
+	}
+	var payload struct {
+		Site    models.Site
+		Account models.Account
+	}
+	payload.Site, _ = sf.FetchSite()
+	payload.Account = a
+	b := &bytes.Buffer{}
+	if err = t.ExecuteTemplate(b, "layout", payload); err != nil {
+		log.Printf("[reactor] renderHome: %+v\n", err)
+		return nil, err
+	}
+
+	return b.Bytes(), nil
 }
